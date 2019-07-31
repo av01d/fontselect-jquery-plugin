8ca25a03f9225e7f59641b81fcd520ae3917fadc
Arjan Haverkamp <arjan@avoid.org>
2019-07-31 09:53:11 +0200

    Version 0.6: Support for local/custom fonts and some bug fixes.

--- jquery.fontselect.js
+++ jquery.fontselect.js
@@ -1,11 +1,13 @@
 /*
- * jQuery.fontselect - A font selector for system fonts and Google Web Fonts
+ * jQuery.fontselect - A font selector for system fonts, local fonts and Google Web Fonts
+ *
  * Made by Arjan Haverkamp, https://www.webgear.nl
  * Based on original by Tom Moor, http://tommoor.com
  * Copyright (c) 2011 Tom Moor, 2019 Arjan Haverkamp
  * MIT Licensed
- * @version 0.5 - 2019-07-24
-*/
+ * @version 0.6 - 2019-07-31
+ * @url https://github.com/av01d/fontselect-jquery-plugin
+ */
 
 (function($){
 
@@ -20,15 +22,17 @@
 			placeholderSearch: 'Search...',
 			searchable: true,
 			lookahead: 2,
-			api: 'https://fonts.googleapis.com/css?family=',
-			systemFonts: 'Arial|Helvetica+Neue|Courier+New|Times+New+Roman|Comic+Sans+MS|Impact'.split('|'),
-			googleFonts: 'Aclonica|Allan|Annie+Use+Your+Telescope|Anonymous+Pro|Allerta+Stencil|Allerta|Amaranth|Anton|Architects+Daughter|Arimo|Artifika|Arvo|Asset|Astloch|Bangers|Bentham|Bevan|Bigshot+One|Bowlby+One|Bowlby+One+SC|Brawler|Buda:300|Cabin|Calligraffitti|Candal|Cantarell|Cardo|Carter+One|Caudex|Cedarville+Cursive|Cherry+Cream+Soda|Chewy|Coda|Coming+Soon|Copse|Corben|Cousine|Covered+By+Your+Grace|Crafty+Girls|Crimson+Text|Crushed|Cuprum|Damion|Dancing+Script|Dawning+of+a+New+Day|Didact+Gothic|Droid+Sans|Droid+Serif|EB+Garamond|Expletus+Sans|Fontdiner+Swanky|Forum|Francois+One|Geo|Give+You+Glory|Goblin+One|Goudy+Bookletter+1911|Gravitas+One|Gruppo|Hammersmith+One|Holtwood+One+SC|Homemade+Apple|Inconsolata|Indie+Flower|Irish+Grover|Istok+Web|Josefin+Sans|Josefin+Slab|Judson|Jura|Just+Another+Hand|Just+Me+Again+Down+Here|Kameron|Kenia|Kranky|Kreon|Kristi|La+Belle+Aurore|Lato|League+Script|Lekton|Limelight|Lobster|Lobster+Two|Lora|Love+Ya+Like+A+Sister|Loved+by+the+King|Luckiest+Guy|Maiden+Orange|Mako|Maven+Pro|Maven+Pro:900|Meddon|MedievalSharp|Megrim|Merriweather|Metrophobic|Michroma|Miltonian+Tattoo|Miltonian|Modern+Antiqua|Monofett|Molengo|Mountains+of+Christmas|Muli:300|Muli|Neucha|Neuton|News+Cycle|Nixie+One|Nobile|Nova+Cut|Nova+Flat|Nova+Mono|Nova+Oval|Nova+Round|Nova+Script|Nova+Slim|Nova+Square|Nunito|Old+Standard+TT|Open+Sans:300|Open+Sans|Open+Sans:600|Open+Sans:800|Open+Sans+Condensed:300|Orbitron|Orbitron:500|Orbitron:700|Orbitron:900|Oswald|Over+the+Rainbow|Reenie+Beanie|Pacifico|Patrick+Hand|Paytone+One|Permanent+Marker|Philosopher|Play|Playfair+Display|Podkova|Press+Start+2P|Puritan|Quattrocento|Quattrocento+Sans|Radley|Raleway:100|Redressed|Rock+Salt|Rokkitt|Ruslan+Display|Schoolbell|Shadows+Into+Light|Shanti|Sigmar+One|Six+Caps|Slackey|Smythe|Sniglet|Sniglet:800|Special+Elite|Stardos+Stencil|Sue+Elen+Francisco|Sunshiney|Swanky+and+Moo+Moo|Syncopate|Tangerine|Tenor+Sans|Terminal+Dosis+Light|The+Girl+Next+Door|Tinos|Ubuntu|Ultra|Unkempt|UnifrakturCook:bold|UnifrakturMaguntia|Varela|Varela+Round|Vibur|Vollkorn|VT323|Waiting+for+the+Sunrise|Wallpoet|Walter+Turncoat|Wire+One|Yanone+Kaffeesatz|Yeseva+One|Zeyada'.split('|')
+			googleApi: 'https://fonts.googleapis.com/css?family=',
+			localFontsUrl: '/fonts/',
+			systemFonts: 'Arial|Helvetica+Neue|Courier+New|Times+New+Roman|Comic+Sans+MS|Verdana|Impact'.split('|'),
+			googleFonts: 'Abril+Fatface|Aclonica|Alfa+Slab+One|Allan|Amarante|Annie+Use+Your+Telescope|Anonymous+Pro|Allerta+Stencil|Allerta|Amaranth|Anton|Arbutus|Architects+Daughter|Archivo+Black|Arimo|Artifika|Arvo|Asset|Astloch|Audiowide|Bangers|Baumans|Bentham|Bevan|Bigshot+One|Black+Ops+One|Bowlby+One|Bowlby+One+SC|Brawler|Bubblegum+Sans|Buda:300|Butcherman|Butterfly+Kids|Cabin|Caesar+Dressing|Calligraffitti|Candal|Cantarell|Cardo|Carter+One|Caudex|Cedarville+Cursive|Changa+One|Cherry+Cream+Soda|Chewy|Coda|Codystar|Comfortaa|Coming+Soon|Copse|Corben|Cousine|Covered+By+Your+Grace|Crafty+Girls|Crimson+Text|Crushed|Cuprum|Damion|Dancing+Script|Dawning+of+a+New+Day|Days+One|Didact+Gothic|Diplomata|Droid+Sans|Droid+Serif|EB+Garamond|Ewert|Expletus+Sans|Faster+One|Fontdiner+Swanky|Forum|Francois+One|Fredoka+One|Fugaz+One|Glass+Antiqua|Geo|Give+You+Glory|Goblin+One|Gorditas|Goudy+Bookletter+1911|Gravitas+One|Gruppo|Hammersmith+One|Hanalei|Henny+Penny|Holtwood+One+SC|Homemade+Apple|Inconsolata|Indie+Flower|Irish+Grover|Istok+Web|Josefin+Sans|Josefin+Slab|Judson|Jura|Just+Another+Hand|Just+Me+Again+Down+Here|Kameron|Kenia|Kranky|Kreon|Kristi|La+Belle+Aurore|Lato|League+Script|Lekton|Limelight|Lobster|Lobster+Two|Lora|Love+Ya+Like+A+Sister|Loved+by+the+King|Luckiest+Guy|Maiden+Orange|Mako|Maven+Pro|Maven+Pro:900|Meddon|MedievalSharp|Megrim|Merriweather|Metrophobic|Michroma|Miltonian+Tattoo|Miltonian|Modern+Antiqua|Monofett|Molengo|Montserrat:300|Montserrat|Montserrat:700|Mountains+of+Christmas|Muli:300|Muli|Mystery+Quest|Neucha|Neuton|News+Cycle|Nixie+One|Nobile|Nova+Cut|Nova+Flat|Nova+Mono|Nova+Oval|Nova+Round|Nova+Script|Nova+Slim|Nova+Square|Nunito|Old+Standard+TT|Open+Sans:300|Open+Sans|Open+Sans:600|Open+Sans:800|Open+Sans+Condensed:300|Orbitron|Orbitron:500|Orbitron:700|Orbitron:900|Oswald|Over+the+Rainbow|Piedra|Prociono|Questrial|Reenie+Beanie|Pacifico|Patrick+Hand|Paytone+One|Permanent+Marker|Philosopher|Play|Playfair+Display|Podkova|Poiret+One|Press+Start+2P|Puritan|Quattrocento|Quattrocento+Sans|Racing+Sans+One|Radley|Raleway:100|Redressed|Ribeye|Ribeye+Marrow|Risque|Rock+Salt|Rokkitt|Ruslan+Display|Schoolbell|Shadows+Into+Light|Shanti|Sigmar+One|Six+Caps|Slackey|Smythe|Sniglet|Sniglet:800|Special+Elite|Stardos+Stencil|Sue+Elen+Francisco|Sunshiney|Swanky+and+Moo+Moo|Syncopate|Tangerine|Tenor+Sans|Terminal+Dosis+Light|The+Girl+Next+Door|Tinos|Ubuntu|Ultra|Unkempt|UnifrakturCook:bold|UnifrakturMaguntia|Varela|Varela+Round|Vast+Shadow|Vibur|Vollkorn|VT323|Waiting+for+the+Sunrise|Wallpoet|Walter+Turncoat|Wire+One|Yanone+Kaffeesatz|Yeseva+One|Zeyada'.split('|')
 		};
 
 		var Fontselect = (function(){
 
 			function Fontselect(original, o) {
 				if (!o.systemFonts) { o.systemFonts = []; }
+				if (!o.localFonts) { o.localFonts = []; }
 				if (!o.googleFonts) { o.googleFonts = []; }
 				this.options = o;
 				this.$original = $(original);
@@ -165,9 +169,10 @@
 						this.$element.on('keyup', __bind(this.keyUp, this));
 						this.$element.addClass('font-select-active');
 						this.$drop.show();
-						this.moveToSelected();
+
 						this.visibleInterval = setInterval(__bind(this.getVisibleFonts, this), 500);
 						this.searchBoxHeight = this.$search.outerHeight();
+						this.moveToSelected();
 
 						/*
 						if (this.options.searchable) {
@@ -222,16 +227,27 @@
 				fontsAsHtml: function() {
 					var i, r, s, style, h = '';
 					var systemFonts = this.options.systemFonts;
+					var localFonts = this.options.localFonts;
 					var googleFonts = this.options.googleFonts;
 
 					for (i = 0; i < systemFonts.length; i++){
 						r = this.toReadable(systemFonts[i]);
 						s = this.toStyle(systemFonts[i]);
 						style = 'font-family:' + s['font-family'];
-						if (googleFonts.length > 0 && i == systemFonts.length-1) {
+						if ((localFonts.length > 0 || googleFonts.length > 0) && i == systemFonts.length-1) {
 							style += ';border-bottom:1px solid #444'; // Separator after last system font
 						}
 						h += '<li data-value="'+ systemFonts[i] +'" data-query="' + systemFonts[i].toLowerCase() + '" style="' + style + '">' + r + '</li>';
+					}
+
+					for (i = 0; i < localFonts.length; i++){
+						r = this.toReadable(localFonts[i]);
+						s = this.toStyle(localFonts[i]);
+						style = 'font-family:' + s['font-family'];
+						if (googleFonts.length > 0 && i == localFonts.length-1) {
+							style += ';border-bottom:1px solid #444'; // Separator after last local font
+						}
+						h += '<li data-value="'+ localFonts[i] +'" data-query="' + localFonts[i].toLowerCase() + '" style="' + style + '">' + r + '</li>';
 					}
 
 					for (i = 0; i < googleFonts.length; i++){
@@ -279,11 +295,14 @@
 					if (fontsLoaded[font]) { return; }
 					fontsLoaded[font] = true;
 
-					if (this.options.systemFonts.indexOf(font) > -1) {
-						// System fonts need not be loaded!
-						return;
+					if (this.options.googleFonts.indexOf(font) > -1) {
+						$('link:last').after('<link href="' + this.options.googleApi + font + '" rel="stylesheet" type="text/css">');
 					}
-					$('link:last').after('<link href="' + this.options.api + font + '" rel="stylesheet" type="text/css">');
+					else if (this.options.localFonts.indexOf(font) > -1) {
+						font = this.toReadable(font);
+						$('head').append("<style> @font-face { font-family:'" + font + "'; font-style:normal; font-weight:400; src:local('" + font + "'), url('" + this.options.localFontsUrl + font + ".woff') format('woff'); } </style>");
+					}
+					// System fonts need not be loaded!
 				}
 			}; // End prototype
 
